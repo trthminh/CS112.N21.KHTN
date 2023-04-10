e5c0924c6f895213b507a4ae3187f3f055fddc6c
Minh Truong Thanh <trthminh1112@gmail.com>
2023-04-10 08:31:15 +0700

    gentest giai tri

---
+++ Assignment/Week4-GenTest/GiaiTri/giaitri.py
@@ -0,0 +1,39 @@
+import random
+import os
+def rand(a=1, b=1000):
+    return random.randrange(a, b)
+
+def mu(a, b):
+    if b == 1:
+        return a
+    if b == 0:
+        return 1
+    tmp = mu(a, b//2)
+    tmp = tmp * tmp
+    if b % 2 == 0:
+        return tmp
+    else:
+        return tmp * a
+
+TestNumber = 25
+for i in range(TestNumber):
+    os.mkdir(f"Test{i}")
+    fi = open(f"./Test{i}/giaitri{i}.inp", "a+")
+    fo = open(f"./Test{i}/giaitri{i}.out", "a+")
+    t = rand(1, 200)
+    fi.write(str(t) + '\n')
+
+
+    for i in range(t):
+        a, b = rand(), rand()
+        fi.write(str(a) + " " + str(b) + '\n')
+        res = mu(a, b) + mu(b, a)
+        fo.write(str(res) + '\n')
+
+    fi.close()
+    fo.close()
+
+
+
+
+
